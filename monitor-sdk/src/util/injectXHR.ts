257ea4fcbd9b31429a140bc729bc45dbb026f3ee
凤之兮原 <1577310596@qq.com>
2022-08-11 15:46:17 +0800

    feat: add catch XHR error

--- monitor-sdk/src/util/injectXHR.ts
+++ monitor-sdk/src/util/injectXHR.ts
@@ -1,51 +1,51 @@
-// export default function injectXHR() {
-//   let XMLHttpRequest = window.XMLHttpRequest;
-//   let oldOpen = XMLHttpRequest.prototype.open;
-//   XMLHttpRequest.prototype.open = function (
-//     method,
-//     url,
-//     async,
-//     username,
-//     password
-//   ) {
+import { httpMetrics } from "@/type";
 
-//     if (!(url as string).match(/logstores/) && !(url as string).match(/sockjs/)) {
-//       this.logData = {
-//         method,
-//         url,
-//         async,
-//         username,
-//         password,
-//       };
-//     }
-//     return oldOpen.apply(this, arguments as any);
-
-//   }
-//   let oldSend = XMLHttpRequest.prototype.send;
-//   let start;
-//   XMLHttpRequest.prototype.send = function (body) {
-//     if (this.logData) {
-//       start = Date.now();
-//       let handler = (type) => (event) => {
-//         let duration = Date.now() - start;
-//         let status = this.status;
-//         let statusText = this.statusText;
-//         tracker.send({
-//           //未捕获的promise错误
-//           kind: "stability", //稳定性指标
-//           type: "xhr", //xhr
-//           eventType: type, //load error abort
-//           pathname: this.logData.url, //接口的url地址
-//           status: status + "-" + statusText,
-//           duration: "" + duration, //接口耗时
-//           response: this.response ? JSON.stringify(this.response) : "",
-//           params: body || "",
-//         });
-//       };
-//       this.addEventListener("load", handler("load"), false);
-//       this.addEventListener("error", handler("error"), false);
-//       this.addEventListener("abort", handler("abort"), false);
-//     }
-//     oldSend.apply(this, arguments);
-//   };
-// };
+// 调用 proxyXmlHttp 即可完成全局监听 XMLHttpRequest
+export const proxyXmlHttp = (sendHandler: Function | null | undefined, loadHandler: Function) => {
+  if ('XMLHttpRequest' in window && typeof window.XMLHttpRequest === 'function') {
+    const oXMLHttpRequest = window.XMLHttpRequest;
+    if (!(window as any).oXMLHttpRequest) {
+      // oXMLHttpRequest 为原生的 XMLHttpRequest，可以用以 SDK 进行数据上报，区分业务
+      (window as any).oXMLHttpRequest = oXMLHttpRequest;
+    }
+    (window as any).XMLHttpRequest = function () {
+      // 覆写 window.XMLHttpRequest
+      const xhr = new oXMLHttpRequest();
+      const { open, send } = xhr;
+      let metrics = {} as httpMetrics;
+      xhr.open = (method, url) => {
+        metrics.method = method;
+        metrics.url = url;
+        open.call(xhr, method, url, true);
+      };
+      xhr.send = (body) => {
+        metrics.body = body || '';
+        metrics.requestTime = new Date().getTime();
+        // sendHandler 可以在发送 Ajax 请求之前，挂载一些信息，比如 header 请求头
+        // setRequestHeader 设置请求header，用来传输关键参数等
+        // xhr.setRequestHeader('xxx-id', 'VQVE-QEBQ');
+        if (typeof sendHandler === 'function') sendHandler(xhr);
+        send.call(xhr, body);
+      };
+      xhr.addEventListener('loadend', () => {
+        // console.log(metrics);
+        const { status, statusText, response } = xhr;
+        metrics = {
+          ...metrics,
+          status,
+          statusText,
+          response,
+          responseTime: new Date().getTime(),
+        };
+        if (typeof loadHandler === 'function') loadHandler(metrics);
+        
+        console.log(metrics);
+        
+        // xhr.status 状态码
+      });
+      // console.log('xhr', xhr);
+      
+      return xhr;
+    };
+  }
+};
