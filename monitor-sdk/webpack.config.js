83e091cb0a3b586486bda8f20549802c98b869d5
凤之兮原 <1577310596@qq.com>
2022-08-22 16:31:37 +0800

    remove: remove htmlPlugin

--- monitor-sdk/webpack.config.js
+++ monitor-sdk/webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {
     CleanWebpackPlugin
 } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // const WebpackDeepScopeAnalysisPlugin = require('webpack-deep-scope-plugin').default;
+
+const isProduction = process.env.NODE_ENV == "production";
 
 const postCssLoaderConfig = {
     loader: 'postcss-loader',
@@ -23,20 +25,22 @@ const postCssLoaderConfig = {
 
 const commonConfig = {
     entry: './src/index.ts',
-    mode: 'development',
     context: process.cwd(),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'monitor.js',
+        environment: {
+            arrowFunction: false
+        },
     },
     // devServer: {
     //     contentBase: path.resolve(__dirname, 'dist')
     // },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.html'),
-            filename: 'index.html'
-        }),
+        // new HtmlWebpackPlugin({
+        //     template: path.resolve(__dirname, './src/index.html'),
+        //     filename: 'index.html'
+        // }),
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
             filename: "static/css/[name].[hash].css",
@@ -57,8 +61,23 @@ const commonConfig = {
                 ]
             }
         }, {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
+            test: /\.ts$/,
+            use: [
+                //配置babel
+                {
+                    //指定加载器
+                    loader: 'babel-loader',
+                    //设置 babel
+                    options: {
+                        //设置预定义的环境
+                        presets: [
+                            //指定环境插件
+                            '@babel/preset-env'
+                        ]
+                    }
+                },
+                'ts-loader'
+            ],
             exclude: /node_modules/
         }, {
             test: /\.css$/,
@@ -86,8 +105,8 @@ const commonConfig = {
         }]
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-        mainFiles: ['index.tsx', 'index'],
+        extensions: ['.ts', '.js'],
+        // mainFiles: ['index.ts', 'index'],
         alias: {
             '@': path.resolve(__dirname, './src'),
             // 'api': path.resolve(__dirname, './src/api'),
@@ -98,4 +117,13 @@ const commonConfig = {
     }
 };
 
-module.exports = commonConfig;
+
+
+module.exports = () => {
+    if (isProduction) {
+        commonConfig.mode = "production";
+    } else {
+        commonConfig.mode = "development";
+    }
+    return commonConfig;
+}
