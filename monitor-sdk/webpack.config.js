fdaa74597b1851e258893fe4afe958d68a004839
凤之兮原 <1577310596@qq.com>
2022-08-24 20:10:37 +0800

    chore: show htmlWebpackPlugin

--- monitor-sdk/webpack.config.js
+++ monitor-sdk/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {
     CleanWebpackPlugin
 } = require('clean-webpack-plugin');
@@ -37,10 +37,10 @@ const commonConfig = {
     //     contentBase: path.resolve(__dirname, 'dist')
     // },
     plugins: [
-        // new HtmlWebpackPlugin({
-        //     template: path.resolve(__dirname, './src/index.html'),
-        //     filename: 'index.html'
-        // }),
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html'
+        }),
         new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
             filename: "static/css/[name].[hash].css",
