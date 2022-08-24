b723c6d8be7b5ab56e0e03652be769b08de7e460
凤之兮原 <1577310596@qq.com>
2022-08-24 23:29:54 +0800

    refactor: refactor BlankScreen

--- monitor-sdk/src/common/blankScreen.ts
+++ monitor-sdk/src/common/blankScreen.ts
@@ -1,21 +1,23 @@
+import { BlackScreenDataType as initData } from "@/type";
 import { isLoad } from "../util";
 
 export function blankScreen(): void {
     console.log('%c%s', 'font-size: 24px; color: orange', '开始监控网页是否白屏');
 
-    const wrapperElements: string[] = ['html', 'body', '#container', '.content.main'];
-    let emptyPoints: number = 0; // 屏幕上空白点的数量
+    // 屏幕上空白点的数量
+    let { wrapperElements, emptyPoints }: initData = initElementsAndPoints();
     const getSelector = function (element) {
         const { id, className, nodeName } = element;
 
         if (id) {
             return `#${id}`;
         } else if (className && typeof className === 'string') {
+            return concatClassName();
+        } else
+            return nodeName ? nodeName.toLowerCase() : element;
+
+        function concatClassName() {
             return `.${className.split(' ').filter(item => !!item).join('.')}`;
-        } else if (nodeName) {
-            return nodeName.toLowerCase();
-        } else {
-            return element;
         }
     }
 
@@ -28,13 +30,7 @@ export function blankScreen(): void {
 
     const judge = function (): void {
         const { innerWidth, innerHeight } = window;
-        for (let i = 1; i <= 9; i++) {
-            const xElements: Element[] = document.elementsFromPoint(innerWidth * i / 10, innerHeight / 2);
-            const yElements: Element[] = document.elementsFromPoint(innerWidth / 2, innerHeight * i / 10);
-
-            isWrapper(xElements);
-            isWrapper(yElements);
-        }
+        findElements();
 
         if (emptyPoints >= 18) {
             const centerElements: Element[] = document.elementsFromPoint(innerWidth / 2, innerHeight / 2);
@@ -43,7 +39,23 @@ export function blankScreen(): void {
         } else {
             console.log('%c%s', 'color: orange', '白屏监控之空白点数量：', emptyPoints);
         }
+
+        function findElements() {
+            for (let i = 1; i <= 9; i++) {
+                const xElements: Element[] = document.elementsFromPoint(innerWidth * i / 10, innerHeight / 2);
+                const yElements: Element[] = document.elementsFromPoint(innerWidth / 2, innerHeight * i / 10);
+
+                isWrapper(xElements);
+                isWrapper(yElements);
+            }
+        }
     }
 
     isLoad(judge);
+
+    function initElementsAndPoints() {
+        const wrapperElements: string[] = ['html', 'body', '#container', '.content.main'];
+        let emptyPoints: number = 0; // 屏幕上空白点的数量
+        return { wrapperElements, emptyPoints };
+    }
 }
