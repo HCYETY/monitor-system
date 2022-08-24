ce4abb829b2cdae5ec219061862e473bf8747484
凤之兮原 <1577310596@qq.com>
2022-08-24 23:28:29 +0800

    refactor: rename

--- monitor-sdk/src/test/index.spec.ts
+++ monitor-sdk/src/test/index.spec.ts
@@ -1,7 +1,8 @@
+import injectConsole from '../common/injectConsole'
 import { describe, expect, it } from 'vitest'
 describe('first', () => {
   it('1 + 1 shoule be 2', () => {
-    expect(1 + 1).toBe(2)
+
   })
 
 })
