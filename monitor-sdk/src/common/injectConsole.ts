3a2e08a70f8ba0c106e256a837a726b36a2a3b57
凤之兮原 <1577310596@qq.com>
2022-08-25 22:48:49 +0800

    refacor: refactor error

--- monitor-sdk/src/common/injectConsole.ts
+++ monitor-sdk/src/common/injectConsole.ts
@@ -1,16 +1,20 @@
+import { ConsoleError } from './../util/ConsoleError'
 
 export default function injectConsole() {
   // ------  console.error  --------
-  let consoleError = window.console.error;
+  let consoleError = window.console.error
   window.console.error = function (error) {
     if (error != '参数有缺失') {
-      const message: string = error.message;
-      const stack = error.stack;
-      const url: string = window.location.href;
-      let row: number = 0, column: number = 0;
+      const { message, stack } = error
+      let column: number = 0
+      let row: number = 0
+      let url = window.location.href
+
+      const consoleErrorData = createConsoleError(message, column, row, stack, url)
+
       if (stack) {
-        let mres = stack.match(/\(.*?\)/g) || [];
-        let firstLine = (mres[0] || "").replace("(", "").replace(")", ""); // 获取到堆栈信息的第一条
+        let mres = stack.match(/\(.*?\)/g) || []
+        let firstLine = (mres[0] || '').replace('(', '').replace(')', '') // 获取到堆栈信息的第一条
 
         // 根据:分隔获取行列
         let info = firstLine.split(':')
@@ -20,17 +24,29 @@ export default function injectConsole() {
 
       // setTimeout(function () {
       // 上报错误内容
-      let opt = {
-        url,
-        row,
-        column,
-        message,
-        stack // 错误堆栈信息
-      }
-      console.log('error捕获', opt);
+      consoleErrorData.row = row
+      consoleErrorData.column = column
+      // let opt = {
+      //   url,
+      //   row,
+      //   column,
+      //   message,
+      //   stack, // 错误堆栈信息
+      // }
+      console.log('error捕获', consoleErrorData)
       // }, 0);
     }
 
-    return consoleError.apply(console, arguments as any);
-  };
-};
+    return consoleError.apply(console, arguments as any)
+
+    function createConsoleError(
+      message: any,
+      column: number,
+      row: number,
+      stack: any,
+      url: string
+    ) {
+      return new ConsoleError(message, column, row, stack, url)
+    }
+  }
+}
--- monitor-sdk/src/util/JsError.ts
+++ monitor-sdk/src/util/JsError.ts
@@ -1,55 +1,17 @@
 export class JsError {
-  private _message
-  private _type
-  private _errorType
-  private _fileName
-  private _position
-  private _selector
+  public message: string
+  public type: string
+  public errorType: string
+  public fileName: string
+  public position: string
+  public selector: string
 
   constructor(message, type, errorType, fileName, position, selector) {
-    this._message = message
-    this._type = type
-    this._errorType = errorType
+    this.message = message
+    this.type = type
+    this.errorType = errorType
     this.fileName = fileName
     this.position = position
-    this._selector = selector
-  }
-
-
-  get message() {
-    return this._message
-  }
-  set message(value) {
-    this._message = value
-  }
-  get type() {
-    return this._type
-  }
-  set type(value) {
-    this._type = value
-  }
-  get errorType() {
-    return this._errorType
-  }
-  set errorType(value) {
-    this._errorType = value
-  }
-  get fileName() {
-    return this._fileName
-  }
-  set fileName(value) {
-    this._fileName = value
-  }
-  get position() {
-    return this._position
-  }
-  set position(value) {
-    this._position = value
-  }
-  get selector() {
-    return this._selector
-  }
-  set selector(value) {
-    this._selector = value
+    this.selector = selector
   }
 }
