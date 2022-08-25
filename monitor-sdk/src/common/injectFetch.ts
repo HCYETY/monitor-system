d26fbb5e1ff6fb733ccdbfe6085df07b7725510e
凤之兮原 <1577310596@qq.com>
2022-08-25 23:12:24 +0800

    refactor: refactor methods

--- monitor-sdk/src/common/injectFetch.ts
+++ monitor-sdk/src/common/injectFetch.ts
@@ -1,108 +1,108 @@
-import { httpMetrics } from "@/type";
-import { calcDuration } from "@/util";
+import { httpMetrics } from '@/type'
+import { calcDuration } from '@/util'
 
 // 调用 proxyFetch 即可完成全局监听 fetch
 export const proxyFetch = (sendHandler: Function | null | undefined, loadHandler: Function) => {
   if (isExistsFetch()) {
+    const _Fetch = getNewFetch()
 
-    const _Fetch = getNewFetch();
-
-    (window as any).fetch = async (input: any, init: RequestInit) => {
+    ;(window as any).fetch = async (input: any, init: RequestInit) => {
       // init 是用户手动传入的 fetch 请求互数据，包括了 method、body、headers，要做统一拦截数据修改，直接改init即可
 
-      if (typeof sendHandler === 'function') sendHandler(init);
-      let metrics = {} as httpMetrics;
+      if (typeof sendHandler === 'function') {
+        sendHandler(init)
+      }
+      let metrics = {} as httpMetrics
 
+      initMetrics()
 
-      initMetrics();
+      return _Fetch
+        .call(window, input, init)
+        .then(async response => {
+          // clone 出一个新的 response,再用其做.text(),避免 body stream already read 问题
+          const { status, statusText, url } = response
+          let message: string
 
-      return _Fetch.call(window, input, init).then(async (response) => {
-        // clone 出一个新的 response,再用其做.text(),避免 body stream already read 问题
-        const { status, statusText, url } = response;
-        let message: string
+          setMessageByStatus()
 
-        setMessageByStatus();
+          const res = response.clone()
+          await setMetrics()
 
-        const res = response.clone();
-        await setMetrics();
-
-        calcDuration(metrics);
-
-        if (typeof loadHandler === 'function') loadHandler(metrics);
-
-        console.log('fetch then', metrics);
-
-
-        return metrics;
-
-        function setMessageByStatus() {
-          if (status >= 200 && status < 300) {
-            // 正常情况
-            message = 'success';
-          } else if (status === 404) {
-            // Not Found
-            message = 'Request failed with status code 404';
-          } else if (status === 500) {
-            // Internal Server Error
-            message = 'Request failed with status code 500';
+          if (typeof loadHandler === 'function') {
+            loadHandler(metrics)
           }
-        }
 
-        async function setMetrics() {
-          metrics = {
-            ...metrics,
-            message,
-            status,
-            statusText,
-            response: await res.text(),
-            responseTime: new Date().getTime(),
-            url
-          };
-        }
-      }).catch(async e => {
-        const { message } = e
+          console.log('fetch then', metrics)
 
-        setErrorMetrics();
+          return metrics
 
-        calcDuration(metrics)
+          function setMessageByStatus() {
+            if (status >= 200 && status < 300) {
+              // 正常情况
+              message = 'success'
+            } else if (status === 404) {
+              // Not Found
+              message = 'Request failed with status code 404'
+            } else if (status === 500) {
+              // Internal Server Error
+              message = 'Request failed with status code 500'
+            }
+          }
 
-        console.log('fetch error', metrics);
+          async function setMetrics() {
+            metrics = {
+              ...metrics,
+              message,
+              status,
+              statusText,
+              response: await res.text(),
+              responseTime: new Date().getTime(),
+              url,
+            }
+            calcDuration(metrics)
+          }
+        })
+        .catch(async e => {
+          const { message } = e
 
-        return metrics
+          setErrorMetrics()
 
+          console.log('fetch error', metrics)
 
-        function setErrorMetrics() {
-          metrics = {
-            ...metrics,
-            message,
-            status: 0,
-            statusText: "error",
-            responseTime: new Date().getTime()
-          };
-        }
-      });
+          return metrics
+
+          function setErrorMetrics() {
+            metrics = {
+              ...metrics,
+              message,
+              status: 0,
+              statusText: 'error',
+              responseTime: new Date().getTime(),
+            }
+            calcDuration(metrics)
+          }
+        })
 
       function initMetrics() {
         metrics = {
           method: init?.method.toUpperCase() || '',
           url: (input && typeof input !== 'string' ? input?.url : input) || '',
           body: init?.body || '',
-          requestTime: new Date().getTime()
-        };
+          requestTime: new Date().getTime(),
+        }
       }
-    };
+    }
   }
 
   function getNewFetch() {
-    const _Fetch = window.fetch;
+    const _Fetch = window.fetch
     if (!(window as any)._Fetch) {
-      (window as any)._Fetch = _Fetch;
+      ;(window as any)._Fetch = _Fetch
     }
-    return _Fetch;
+    return _Fetch
   }
 
   function isExistsFetch() {
-    return 'fetch' in window && typeof window.fetch === 'function';
+    return 'fetch' in window && typeof window.fetch === 'function'
   }
-};
-
+}
